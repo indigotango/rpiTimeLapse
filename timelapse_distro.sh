785cee046e0d11bacead25241d974c3cc6ace9ea
indigotango <indigotango@users.noreply.github.com>
2018-05-20 17:27:41 -0800

    Update timelapse_distro.sh
    Add "pretty timestamp" for caption of Telegram posting of the generated video

--- timelapse_distro.sh
+++ timelapse_distro.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # Generate timestamp of previous day
-timestamp=$(date --date="1 days ago" "+%Y%m%d")
+timestamp=$(date --date="1 days ago" "+%Y%m%d") # 20180519
+timestampPretty=$(date --date="1 days ago" "+%A, %d %b %Y")# Saturday, 19 May 2018
 
 # Ensure script is running within user's home directory
 cd /home/user
 
 # Upload generated video file to Telegram group
 # Telegram group ID is hard coded into URL!
-curl -s -X POST "https://api.telegram.org/bot<bot API key>/sendVideo" -F chat_id=-<group ID after - symbol> -F video="@timelapse_$timestamp.mp4" -F caption="Yesterday's timelapse"
+curl -s -X POST "https://api.telegram.org/bot<bot API key>/sendVideo" -F chat_id=-<group ID after - symbol> -F video="@timelapse_$timestamp.mp4" -F caption="Timelapse for $timestampPretty"
 
 # Upload generated video file to file server
 rsync --remove-source-files ~/timelapse_$timestamp.mp4 <file server user>@<file server IP>:<file server path>
